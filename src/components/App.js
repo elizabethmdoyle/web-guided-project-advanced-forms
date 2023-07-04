9d1d5ba10ed66e38172799cf5a4caa642c91049d
Elizabeth Doyle <myoddjourney@gmail.com>
2023-07-04 17:39:44 -0400

    working on project

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ import FriendForm from './FriendForm'
 // 🔥 STEP 2- FLESH OUT FriendForm.js
 // 🔥 STEP 3- FLESH THE SCHEMA IN ITS OWN FILE
 // 🔥 STEP 4- IMPORT THE SCHEMA, AXIOS AND YUP
-
+import axios from 'axios'
+import schema from '../validation/formSchema'
+import * as yup from 'yup'
 
 //////////////// INITIAL STATES ////////////////
 //////////////// INITIAL STATES ////////////////
@@ -48,12 +50,26 @@ export default function App() {
   const getFriends = () => {
     // 🔥 STEP 5- IMPLEMENT! ON SUCCESS PUT FRIENDS IN STATE
     //    helper to [GET] all friends from `http://buddies.com/api/friends`
+      axios.get(`http://buddies.com/api/friends`)
+       .then(res => setFriends(res.data))
+       .catch(err => console.error(Err))
+ `` }
   }
 
   const postNewFriend = newFriend => {
     // 🔥 STEP 6- IMPLEMENT! ON SUCCESS ADD NEWLY CREATED FRIEND TO STATE
     //    helper to [POST] `newFriend` to `http://buddies.com/api/friends`
     //    and regardless of success or failure, the form should reset
+
+    useEffect(() => {
+        axios.post(`http://buddies.com/api/friends`)
+        .then(res => 
+          setFriends([res.data, ...friends])
+          setFormValues(initialFormValues)
+        
+        )
+        .catch(err => console.error(Err))
+    }, [])
   }
 
   //////////////// EVENT HANDLERS ////////////////
@@ -74,20 +90,28 @@ export default function App() {
       role: formValues.role.trim(),
       civil: formValues.civil.trim(),
       // 🔥 STEP 7- WHAT ABOUT HOBBIES?
+
     }
     // 🔥 STEP 8- POST NEW FRIEND USING HELPER
+
+    postNewFriend(newFriend)
+
   }
 
   //////////////// SIDE EFFECTS ////////////////
   //////////////// SIDE EFFECTS ////////////////
   //////////////// SIDE EFFECTS ////////////////
+
+
   useEffect(() => {
     getFriends()
   }, [])
 
   useEffect(() => {
     // 🔥 STEP 9- ADJUST THE STATUS OF `disabled` EVERY TIME `formValues` CHANGES
-  }, [])
+
+
+  }, [formValues])
 
   return (
     <div className='container'>
--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,23 @@
 // Here goes the schema for the form
+import * as Yup from 'yup'
+
+const formSchema = Yup.object().shape({
+    username: Yup
+        .string('Username is required')
+        .required('Please enter a name')
+    email: Yup
+        .string('Must be a valid email address')
+        .required('Must be a valid email address')
+       
+        role: Yup
+        .dropdown('A role must be selected')
+        .required('Must be a valid email address')
+        civil: Yup
+        .checkbox('A civil option must be selected')
+        .required('Must select an option')
+        
+
+
+
+
+})
